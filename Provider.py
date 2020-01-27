5c816fe597e7f567bb6ea71f706249158f28d2cd
prateekazam <prateekazam@gmail.com>
2020-01-26 19:13:41 -0500

    Added pprint codes

--- Provider.py
+++ Provider.py
@@ -1,4 +1,5 @@
 import os as os  # os package help python code to communicate operating system.
+import pprint  as pprint
 
 
 # https://docs.python.org/3/library/os.html
@@ -22,7 +23,26 @@ class Provider:
         """ list function will execute mulitpass exec command """
         os.system(f"multipass exec {command}")
 
+    def print_os(self):
+        """function to print os name"""
+        print(os.name)
+        return os.name
+
+    def ram_sapce(self):
+        """function to return RAM memory """
+        mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
+        mem_gib = mem_bytes / (1024 ** 3)
+        return mem_gib
+
+    def disk_space(self):
+        """function to return HDD memory """
+        s = os.statvfs('/')
+        total = (s.f_bavail * s.f_frsize) / (1024 ** 3)
+        return total
+
 
 if __name__ == "__main__":  # Python will start running code from here.
     p = Provider()  # creating instance of Provider class
-    p.list()  # Calling list function.
+    # p.list()  # Calling list function.
+    values = {'os_name' : p.print_os(),'os_ram': p.ram_sapce(), 'os_hdd': p.disk_space()}
+    pprint.pprint(values,width=1)
